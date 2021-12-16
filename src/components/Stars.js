15d36daf2d9623263ac6165824b9d9e4b54ad666
Mark Bucholski <mbucholski95@gmail.com>
2021-12-16 15:51:20 -0500

    fifth commit

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -2,10 +2,9 @@ const stars = () => {
 
     const starMultiplier = () => {
         let stars = [];
-        for (let i = 0; i < 100; i++) {
-            stars.push(<div className='stars' key={i}></div>);
+        for (let i = 0; i < 250; i++) {
+            stars.push(<div className={`star--${i + 1} star--${i + 1}__twinkle star--${i + 1}__shooting`} key={i + 1}></div>);
         }
-        <div className='stars'></div>
         return <div>{stars}</div>
     }
 
