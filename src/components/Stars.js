88f804e63688f5af57c85ac35856ed7eeb35621e
Mark Bucholski <mbucholski95@gmail.com>
2021-12-16 19:33:14 -0500

    sixth commit

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,9 +1,18 @@
-const stars = () => {
+import React, {useEffect} from 'react';
+
+const Stars = () => {
+
+    useEffect(() => {
+        setTimeout(() => setInterval(() => {
+            console.log('hi');
+            return () => clearInterval();
+        }, 3000), 9500);
+    }, [])
 
     const starMultiplier = () => {
         let stars = [];
         for (let i = 0; i < 250; i++) {
-            stars.push(<div className={`star--${i + 1} star--${i + 1}__twinkle star--${i + 1}__shooting`} key={i + 1}></div>);
+            stars.push(<div className={`star--${i + 1} star--${i + 1}__animation`} key={i + 1}></div>);
         }
         return <div>{stars}</div>
     }
@@ -13,4 +22,4 @@ const stars = () => {
     )
 }
 
-export default stars;
+export default Stars;
