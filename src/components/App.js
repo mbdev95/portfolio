9ab8afd8ce7d37ea218937167257e0b5b236236f
Mark Bucholski <mbucholski95@gmail.com>
2021-12-16 22:42:28 -0500

    eigth commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
+import React, {Component} from 'react';
 import '../scss/App.scss';
 import Stars from './Stars.js';
 
-const App = () => {
-  return (
-    <>
-      <Stars />
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Stars />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Stars.js
+++ src/components/Stars.js
@@ -4,15 +4,19 @@ const Stars = () => {
 
     useEffect(() => {
         setTimeout(() => setInterval(() => {
-            console.log('hi');
-            return () => clearInterval();
-        }, 3000), 9500);
+            for (let i = 1; i < 51; i++) {
+                const randomNumber = Math.floor(Math.random() * 250) + 1;
+                document.getElementsByClassName(`star--${randomNumber}`)[0].classList.add(`star--${randomNumber}__shooting`);
+                setTimeout(() => document.getElementsByClassName(`star--${randomNumber}`)[0].classList.remove(`star--${randomNumber}__shooting`), 3000);
+            }
+        }, 3000), 6000);
+        return () => clearInterval();
     }, [])
 
     const starMultiplier = () => {
         let stars = [];
-        for (let i = 0; i < 250; i++) {
-            stars.push(<div className={`star--${i + 1} star--${i + 1}__animation`} key={i + 1}></div>);
+        for (let i = 1; i < 251; i++) {
+            stars.push(<div className={`star--${i} star--${i}__animation`} key={i}></div>);
         }
         return <div>{stars}</div>
     }
