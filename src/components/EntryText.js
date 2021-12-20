ca80f8c1b67d977969f2ee55888849c2e6898ca0
Mark Bucholski <mbucholski95@gmail.com>
2021-12-19 22:59:50 -0500

    fifteenth commit

--- src/components/EntryText.js
+++ src/components/EntryText.js
@@ -4,16 +4,28 @@ const EntryText = () => {
 
     const fadeTextIn = () => {
         document.getElementsByClassName('entryText')[0].classList.add('entryText__fade-in');
+        const introLetters = document.querySelectorAll('.entryText span');
+        for ( let i = 0; i < introLetters.length; i++ ) {
+            const location = introLetters[i].getBoundingClientRect();
+            introLetters[i].style.setProperty('--top-entry-letter', location.top + 'px');
+            introLetters[i].style.setProperty('--left-entry-letter', location.left + 'px');
+        }
+    }
+
+    const fadeTextOut = () => {
+        const introLetters = document.querySelectorAll('.entryText span');
+        const randomX = Math.floor(Math.random() * 90) + 10;
+        const randomY = Math.floor(Math.random() * 90) + 10;
+        for ( let i = 0; i < introLetters.length; i++ ) {
+            introLetters[i].classList.add(`entryText--${i + 1}__rotate-away`);
+            introLetters[i].style.setProperty('--translate-x', randomX + '%');
+            introLetters[i].style.setProperty('--translate-y', randomY + '%');
+        }
     }
 
     useEffect(() => { 
         fadeTextIn();
-        setTimeout(() => {
-            const introLettersAmount = document.querySelectorAll('.entryText span').length;
-            for ( let i = 0; i < introLettersAmount; i++ ) {
-                document.querySelectorAll('.entryText span')[i].classList.add(`entryText--${i + 1}__rotate-away`);
-            }
-        }, 3500); 
+        setTimeout(() => fadeTextOut(), 4500); 
     }, []);
 
     return (
