24abb8f20256816899478819b6c3c529933d912d
Mark Bucholski <mbucholski95@gmail.com>
2021-12-22 23:38:31 -0500

    twenty-third commit

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,8 @@
-import {useEffect, useContext} from "react";
+import {useEffect, useContext} from 'react';
 import {pageInfoContext} from '../context';
+import linkedIn from '../../img/socialMediaIcons/linkedin.png';
+import GitHub from '../../img/socialMediaIcons/github.png';
+import resume from '../../img/socialMediaIcons/resume.png';
 
 const Home = () => {
 
@@ -11,10 +14,17 @@ const Home = () => {
 
     return (
         <>
-            <div className="home">
-                <h1>This is the Home Page</h1>
-                <button type="button" onClick={event => slide(event.target.textContent.toLowerCase(), 'home')}>Portfolio</button>
-                <button type="button" onClick={event => slide(event.target.textContent.toLowerCase(), 'home')}>About</button>
+            <div className='home'>
+                <h2>Mark Bucholski</h2>
+                <p>Software engineer from a galaxy up north</p>
+                <p>Educated with a diploma in front-end web development and being a self-learner.</p>
+                <div className='socialMedia'>
+                    <img src={linkedIn} alt='LinkedIn Logo'/>
+                    <img src={GitHub} alt='GitHub Logo'/>
+                    <img src={resume} alt='Resume Icon'/>
+                </div>
+                <button type='button' onClick={event => slide(event.target.textContent.toLowerCase(), 'home')}>Portfolio</button>
+                <button type='button' onClick={event => slide(event.target.textContent.toLowerCase(), 'home')}>About</button>
             </div>
         </>
     )
--- src/components/pages/portfolio/ExpressFood.js
+++ src/components/pages/portfolio/ExpressFood.js
@@ -1,10 +1,14 @@
 import mysqlLogo from '../../../img/programmingLogos/mysql.jpg';
+import ExpressFoodMedium from '../../../img/applications/medium/ExpressFoodMedium.png';
+import ExpressFoodLarge from '../../../img/applications/large/ExpressFoodLarge.png';
+import { useContext } from 'react';
+import { pageInfoContext } from '../../context';
 
 const ExpressFood = () => {
     return (
         <div className='portfolio--project'>
             <h2>Express Food Database</h2>
-            <img src={mysqlLogo} alt='MySQL Logo'/>
+            {useContext(pageInfoContext).projectPhotoSelector(<img src={mysqlLogo} alt='MySQL Logo'/>, <img src={ExpressFoodMedium} alt='Express Food UML Class Diagram'/>, <img src={ExpressFoodLarge} alt='Express Food UML Class Diagram'/>)}
             <p>I created a database for the fictitious food delivery company, ExpressFood, using UML diagrams and MySQL. Additionally, I queried my database using MySQL.</p>
             <h3>Skills Used:</h3>
             <ul>
